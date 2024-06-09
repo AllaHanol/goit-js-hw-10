58364c107a7970970d5f36b5e726757dbd2e486a
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-06-10 00:08:19 +0200

    erneut

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,69 +1,102 @@
 'use strict';
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import { saveToLS, loadFromLS } from './helper.js';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import '../css/timer.css';
+import '../img/izitoast/bi_x-octagon.png'
 
-
-  
 const refs = {
-    datetimePicker:document.querySelector('#datetime-picker'),
-    startBtn:document.querySelector('[data-start]'),
-    days:document.querySelector('[data-days]'),
-    hours:document.querySelector('[data-hours]'),
-    minutes:document.querySelector('[data-minutes]'),
-    seconds:document.querySelector('[data-seconds]'),
-}
-let intervalId;
-refs.startBtn.addEventListener('click', ()=>{
-    const initTime = Date.now(); //catch from input
-
-    intervalId = setInterval( ()=>{
-        const currentTime = Date.now();
-        const diff = initTime-currentTime;
-        const time = convertMs(diff);
-        
-});
-setTimeout(()=>{
-    clearInterval(intervalId);
-}, initTime-Date.now ());
+  datetimePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
+// let intervalId;
+let initTime;
 
 refs.startBtn.disabled = true;
+
+
+refs.startBtn.addEventListener('click', (e) => {
+    console.log(e.target);
+  const intervalId = setInterval(() => {
+    renderTime(initTime)
+  }, 1000);
+  //скидання інтервалу після проходження
+  setTimeout(() => {
+    clearInterval(intervalId);
+  }, initTime - Date.now());
 });
 
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      }
-      
-      console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-      console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-      console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-  
-// },1000);
-// });
-// const options = {
-//     enableTime: true,
-//     time_24hr: true,
-//     defaultDate: new Date(),
-//     minuteIncrement: 1,
-//     onClose(selectedDates) {
-//       console.log(selectedDates[0]);
-//     },
-//   };
 
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function renderTime(time) {
+  const diff = initTime - Date.now();
+  const obj = convertMs(diff);
+  console.log(obj);
+  refs.daysEl.innerText = String(obj.days).padStart(2, '0');
+  refs.hoursEl.innerText = String(obj.hours).padStart(2, '0');
+  refs.minutesEl.innerText = String(obj.minutes).padStart(2, '0');
+  refs.secondsEl.innerText = String(obj.seconds).padStart(2, '0');
+}
+function validateTime (time){
+    return time <= Date.now()
+
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    initTime = selectedDates[0];
+    
+    if (validateTime(initTime)) {
+        
+        iziToast.show({
+            position: 'topRight',
+            title: 'Error',
+            titleColor: '#fff',
+            titleSize: '16',
+            titleLineHeight: '24',
+            message: "Please choose a date in the future",
+            messageColor: '#fff',
+            messageSize: '16',
+            messageLineHeight: '24',
+            progressBarColor: '#B51B1B',                   
+            backgroundColor: '#EF4040',
+            image: '../img/izitoast/bi_x-octagon.png',
+            imageWidth: 24,
+           
+    })
+        return
+    } else {
+        renderTime(initTime);
+        refs.startBtn.disabled = false;
+        refs.startBtn.classList.add('button-normal');
+    }
+  },
+};
+flatpickr('.flatpickr-input', options);
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,34 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import { saveToLS, loadFromLS } from './helper.js';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const delay = parseInt(form.elements.delay.value);
+  const state = form.elements.state.value;
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `:white_check_mark: Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `:x: Rejected promise in ${delay}ms`,
+      });
+    });
+});
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
