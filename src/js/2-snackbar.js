c3043475e3386554a4cf56bed43f9eba128127c7
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-06-10 02:33:20 +0200

    erneut

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,54 +1,63 @@
 'use strict';
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import '../css/snackbar.css'
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import '../css/snackbar.css';
 import { saveToLS, loadFromLS } from './helper.js';
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const delay = parseInt(form.elements.delay.value);
-  const state = form.elements.state.value;
-  createPromise(delay, state)
-    .then((delay) => {
-      iziToast.show({
-        position: 'topRight',
-        title: 'Success',
-        titleColor: '#fff',
-        titleSize: '16',
-        titleLineHeight: '24',
-        message: '`✅ Fulfilled promise in ${delay}ms`',
-        messageColor: '#fff',
-        messageSize: '16',
-        messageLineHeight: '24',
-        progressBarColor: '#326101',                   
-        backgroundColor: '#59A10D',
-      });
-    })
-    .catch((delay) => {
-      iziToast.show({
-        position: 'topRight',
-        title: 'Error',
-        titleColor: '#fff',
-        titleSize: '16',
-        titleLineHeight: '24',
-        message: '`❌ Rejected promise in ${delay}ms`',
-        messageColor: '#fff',
-        messageSize: '16',
-        messageLineHeight: '24',
-        progressBarColor: '#B51B1B',                   
-        backgroundColor: '#EF4040',
-      });
+
+  const inputEl = document.querySelector('.form');
+  
+  
+  inputEl.addEventListener('submit', promiseFoo);
+  
+  function promiseFoo(e) {
+    e.preventDefault();
+  
+    let delay = e.target.delay.value;
+    let status = e.target.state.value
+    
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (status === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay)
+        }
+      }, delay);
+      console.log(status);
     });
-});
-function createPromise(delay, state) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
+  }
+  
+    promise
+      .then(delay => {
+        iziToast.success({
+          message: `Fulfilled promise in ${delay} ms`,
+          messageSize: '16',
+          messageColor: '#fff',
+          backgroundColor: '#59a10d',
+          position: 'topRight',
+          close: true,
+          closeOnEscape: true,
+          closeOnClick: true,
+          progressBar: true,
+          progressBarColor: '#326101',
+          iconColor: '#fff',
+          iconUrl: successImageUrl,
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          message: `Rejected promise in ${delay}ms`,
+          messageSize: '16',
+          messageColor: '#fff',
+          backgroundColor: '#ef4040',
+          position: 'topRight',
+          close: true,
+          closeOnEscape: true,
+          closeOnClick: true,
+          progressBar: true,
+          progressBarColor: '#ffbebe',
+          iconUrl: imageUrl,
+          iconColor: '#fff',
   });
-}
+      });
