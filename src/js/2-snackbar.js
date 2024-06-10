63865018fb00e07100551da259ee34e434a3a39f
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-06-10 21:45:43 +0200

    snacbar settings

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,62 +2,61 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import '../css/snackbar.css';
-import { saveToLS, loadFromLS } from './helper.js';
+import imageUrl from '../img/izitoast/bi_check2-circle.png'
+import imageErrUrl from '../img/izitoast/bi_x-octagon.png'
 
 
-  const inputEl = document.querySelector('.form');
-  
-  
-  inputEl.addEventListener('submit', promiseFoo);
-  
-  function promiseFoo(e) {
-    e.preventDefault();
-  
-    let delay = e.target.delay.value;
-    let status = e.target.state.value
-    
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (status === 'fulfilled') {
-          resolve(delay);
-        } else {
-          reject(delay)
-        }
-      }, delay);
-      console.log(status);
-    });
-  }
-  
-    promise
-      .then(delay => {
-        iziToast.success({
-          message: `Fulfilled promise in ${delay} ms`,
-          messageSize: '16',
-          messageColor: '#fff',
-          backgroundColor: '#59a10d',
-          position: 'topRight',
-          close: true,
-          closeOnEscape: true,
-          closeOnClick: true,
-          progressBar: true,
-          progressBarColor: '#326101',
-          iconColor: '#fff',
-          iconUrl: successImageUrl,
-        });
-      })
-      .catch(delay => {
-        iziToast.error({
+      const inputEl = document.querySelector('.form').addEventListener('submit', promiseFoo)
+      function promiseFoo(e) {
+        e.preventDefault(); // Зупиняємо стандартну поведінку форми
+
+        const delay = e.target.delay.value;
+        const state = document.querySelector('input[name="state"]:checked').value;
+
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (state === 'fulfilled') {
+                    resolve(delay);
+                } else {
+                    reject(delay);
+                }
+            }, delay);
+        })
+        .then((delay) => {
+          iziToast.success({
+            message: `Fulfilled promise in ${delay} ms`,
+            messageColor: '#fff',
+            messageSize: '16',
+            messageLineHeight: '24',
+            title: 'OK',
+            titleColor: '#fff',
+            titleSize: '16',
+            titleLineHeight: '24',
+            backgroundColor: '#59A10D',
+            position: 'topRight',
+            close: true,
+            closeOnEscape: true,
+            closeOnClick: true,
+            progressBar: true,
+            progressBarColor: '#326101',
+            iconUrl: imageUrl,
+            imageWidth: 24, 
+          })
+       })
+        .catch((delay) => {
+          iziToast.error({
           message: `Rejected promise in ${delay}ms`,
           messageSize: '16',
           messageColor: '#fff',
-          backgroundColor: '#ef4040',
+          backgroundColor: '#EF4040',
           position: 'topRight',
           close: true,
           closeOnEscape: true,
           closeOnClick: true,
           progressBar: true,
-          progressBarColor: '#ffbebe',
-          iconUrl: imageUrl,
-          iconColor: '#fff',
-  });
-      });
+          progressBarColor: '#B51B1B',
+          iconUrl: imageErrUrl,
+          imageWidth: 24,
+          })
+        });
+    };
